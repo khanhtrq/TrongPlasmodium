fa2fa6decf40aed0ecee52c0c8be11b62de86f3f
Luong Duc Trong <trong.ldwork@gmail.com>
2025-06-05 02:08:07 +0700

    refactor: update model configuration and enhance augmentation strategies

--- src/augment.py
+++ src/augment.py
@@ -313,8 +313,12 @@ class TimmAugmentationStrategy:
                     p=0.2,
                 ),
                 transforms.RandomAdjustSharpness(
-                    sharpness_factor=0.5,
-                    p=0.2
+                    sharpness_factor= 5.0,
+                    p=0.5
+                ),
+                transforms.GaussianBlur(
+                    kernel_size=(5, 5),
+                    sigma=(0.1, 2.0),
                 ),    
                 transforms.RandomAffine(
                     degrees=0,
@@ -325,8 +329,8 @@ class TimmAugmentationStrategy:
                 transforms.ToTensor(),
                 transforms.Normalize(mean=self.mean, std=self.std),
                 transforms.RandomErasing(
-                    p=0.1,
-                    scale=(0.02, 0.1),
+                    p=0.3,
+                    scale=(0.02, 0.05),
                     ratio=(0.3, 3.3),
                     value=0
                 ),
